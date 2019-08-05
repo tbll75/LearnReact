316d1ef9a0a4caac2801ef28309082e61b2708e5
Thibault LOUIS-LUCAS <thib@MacBook-Pro-de-Thibault.local>
2019-08-05 21:56:47 +0200

    Add Favorites tab

---
+++ Components/Favorites.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Text, StyleSheet} from 'react-native'
+
+class Favorites extends React.Component
+{
+
+    render () {
+        return (
+            <Text>Mes favoris</Text>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default Favorites
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -1,6 +1,9 @@
-import { createStackNavigator, createAppContainer } from 'react-navigation'
+import { createStackNavigator, createAppContainer, createBottomTabNavigator } from 'react-navigation'
 import Search from '../Components/Search.js'
 import FilmDetail from '../Components/FilmDetail.js'
+import Favorites from '../Components/Favorites.js'
+import React from 'react'
+import {Image, StyleSheet} from 'react-native'
 
 const SearchStackNavigator = createStackNavigator(
     {
@@ -17,4 +20,46 @@ const SearchStackNavigator = createStackNavigator(
     }
 )
 
-export default createAppContainer(SearchStackNavigator)
+const MoviesTabNavigator = createBottomTabNavigator(
+    {
+        Search: {
+            screen: SearchStackNavigator,
+            navigationOptions: {
+                tabBarIcon: () => {
+                    return <Image 
+                        source={require("../Images/search.png")}
+                        style={styles.icon}/>
+                }
+            }
+        },
+        Favorites: {
+            screen: Favorites,
+            navigationOptions: {
+                tabBarIcon: () => {
+                    return <Image 
+                        source={require("../Images/fav_activated.png")}
+                        style={styles.icon}/>
+                }
+            }
+        },
+
+    },
+    {
+        tabBarOptions: {
+            showLabel: false,
+            showIcon: true,
+            activeBackgroundColor: '#DDDDDD',
+            inactiveBackgroundColor: '#FFFFFF'
+
+        }
+    }
+)
+
+const styles = StyleSheet.create({
+    icon: {
+        width: 30,
+        height: 30
+    }
+})
+
+export default createAppContainer(MoviesTabNavigator)
